81a845e24db169d060eddf11a365f06038d324ba
Ben Veenema <bveenema@gmail.com>
2019-05-22 11:23:56 -0400

    Catch Mock Serial up to current development WIP

--- mockSerialPort.js
+++ mockSerialPort.js
@@ -9,6 +9,76 @@ const Devices = [
 			vendorId: '2B04',
 			productId: 'C006' 
 		},
+		config: {
+			baud: 9600,
+			settings: [
+				{
+					name: "Volume",
+					type: "slider",
+					command: 4,
+					min: 20,
+					max: 30,
+					default: 20,
+					float: false,
+					unit: "dB"
+				},
+				{
+					name: "Volume",
+					type: "slider",
+					command: 4,
+					min: 0,
+					max: 10,
+					default: 1,
+					float: true,
+					floatPrecision: 2,
+					unit: "dB"
+				},
+				{
+					name: "Motor Seed",
+					type: "input",
+					command: 0,
+					min: 0,
+					max: 10,
+					default: 1,
+					float: false,
+					unit: "RPM"
+				},
+				{
+					name: "Target Pressure",
+					type: "input",
+					command: 1,
+					min: 10,
+					max: 500,
+					default: 11,
+					float: false,
+					unit: "PSI"
+				},
+				{
+					name: "Direction",
+					type: "picker",
+					command: 2,
+					default: "Left",
+					options: [
+						"Left",
+						"Right",
+						"Up",
+						"Down"
+					]
+				},
+				{
+					name: "Direction",
+					type: "picker",
+					command: 3,
+					default: "Left",
+					options: [
+						"Left",
+						"Right",
+						"Up",
+						"Down"
+					]
+				}
+			] 
+		}
 	},
 	{
 		deviceInfo: { 
@@ -38,13 +108,33 @@ const events = require('events')
 class SerialPort extends events.EventEmitter{
 	constructor(comName){
 		super()
+
+		// save the device info
 		this.deviceInfo = Devices.find((device) => {
 			return comName === device.deviceInfo.comName
-		})
+		}).deviceInfo
+
+		// save the device config
+		this.config = Devices.find((device) => {
+			return comName === device.deviceInfo.comName
+		}).config
+
+		// Create the open event
 		setTimeout(function(){ 
-			console.log('Emitting OPEN event')
 			this.emit('open') 
-		}.bind(this), 5)
+		}.bind(this), 50)
+
+		// Send the READY message
+		this.ready = false
+		setTimeout(function(){
+			this.emit('data', "READY")
+		}.bind(this), 200)
+
+		// Drip data
+		this.dataDripCount = 0
+		this.dataDripInterval = setInterval(function(){
+			this.emit('data', this.deviceInfo.comName + ': ' + this.dataDripCount++)
+		}.bind(this), 1000)
 	}
 
 	static list() {
@@ -54,6 +144,30 @@ class SerialPort extends events.EventEmitter{
 			})
 			resolve(deviceList)
 		})
+	}
+
+	write(data) {
+		if(data === 'CONFIG'){
+			console.log('Sending Config')
+			let config = JSON.stringify(this.config)
+			setTimeout(function(){ this.emit('data', config) }.bind(this), 50)
+		}else{
+			setTimeout(function(){ this.emit('data', data) }.bind(this), 10)
+		}
+	}
+
+	update(){}
+
+	// Mock the pipe function. Returns the object so that parser events can be registered on the instance
+	pipe(){
+		return this
+	}
+
+	// Close - Clean up the instance before GC
+	// Disables the data drip
+	close(callback){
+		clearInterval(this.dataDripInterval)
+		callback()
 	}
 }
 
