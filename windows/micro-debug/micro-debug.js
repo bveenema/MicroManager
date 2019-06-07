971727ed901305c9f9a4a6090b987ad6b23599ae
Ben Veenema <bveenema@gmail.com>
2019-06-07 13:31:38 -0400

    prevent duplicate windows

--- windows/micro-debug/micro-debug.js
+++ windows/micro-debug/micro-debug.js
@@ -4,17 +4,24 @@ const url = require('url')
 const path = require('path')
 const {BrowserWindow} = electron;
 
-let microDebugWindow;
+let window;
 let currentTheme;
 
 // Handle Create Micro Debug window
 function createMicroDebugWindow(){
+	// prevent duplicate windows
+	if(window) {
+		window.show()
+		return
+	}
+
 	// create new window
-	microDebugWindow = new BrowserWindow({
+	window = new BrowserWindow({
 		autoHideMenuBar: true,
 		width: 400,
 		height: 600,
 		title: 'Micro Data',
+		titleBarStyle: 'hidden',
 		webPreferences: {
 			nodeIntegration: true
 		},
@@ -22,39 +29,39 @@ function createMicroDebugWindow(){
 	})
 
 	// load html into window
-	microDebugWindow.loadURL(url.format({
+	window.loadURL(url.format({
 		pathname: path.join(__dirname, 'microDebugWindow.html'),
 		protocol: 'file:',
 		slashes: true
 	}))
 
 	// When the window is rendered, update the theme and show
-	microDebugWindow.once('ready-to-show', () => {
-		microDebugWindow.webContents.send('theme:change', currentTheme)
-		microDebugWindow.show()
+	window.once('ready-to-show', () => {
+		window.webContents.send('theme:change', currentTheme)
+		window.show()
 	})
 
 	// Garbage collection handle
-	microDebugWindow.on('close', function(){
-		microDebugWindow=null
+	window.on('close', function(){
+		window=null
 	})
 
-	console.log('microDebugWindow: ',microDebugWindow)
+	console.log('microDebugWindow: ',window)
 }
 
 module.exports = {
 	Create: createMicroDebugWindow,
 
 	Update: function(data){
-		if(microDebugWindow){
-				microDebugWindow.webContents.send('micro:debug', data)
+		if(window){
+				window.webContents.send('micro:debug', data)
 		}
 	},
 
 	Theme: function(theme){
 		currentTheme = theme
-		if(microDebugWindow){
-			microDebugWindow.webContents.send('theme:change', theme)
+		if(window){
+			window.webContents.send('theme:change', theme)
 		}
 	}
 }
